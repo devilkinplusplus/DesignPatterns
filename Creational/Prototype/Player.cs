7032944889aaf083e5928a690a9c8d2bfce0c49a
Ilkin Rufullayev <rufullayevilkin66@gmail.com>
2023-06-07 22:43:19 +0400

    Prototype dp

---
+++ Creational/Prototype/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Player : ProtoPlayer
+    {
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public byte KitNumber { get; set; }
+        public bool IsCaptain { get; set; }
+        public Position Position { get; set; }
+
+        public Player(string name, string team, byte kitNumber, bool isCaptain, Position position)
+        {
+            Name = name;
+            Team = team;
+            KitNumber = kitNumber;
+            IsCaptain = isCaptain;
+            Position = position;
+        }
+
+        public override ProtoPlayer Clone()
+        {
+            return this.MemberwiseClone() as ProtoPlayer;
+        }
+    }
+
+    public enum Position
+    {
+        GoalKeeper,
+        Defender,
+        Midfielder,
+        Forward
+    }
+
+}
---
+++ Creational/Prototype/Program.cs
@@ -0,0 +1,12 @@
+﻿//Yeni obyekt yaratmaq maliyyetlidir, her defe new ile constructora parametrler gondermek lazimdir
+//Bunun evezine obyekti kopyalaya bilerik, kopyalanan zaman heapdaki adresleri ferqli olacaqdir
+
+using Prototype;
+
+Player player1 = new Player("Kevin De Bruyne", "Manchester City", 17, false, Position.Midfielder);
+
+Player player2 = (Player)player1.Clone();
+
+if (player1.Equals(player2))
+    Console.WriteLine("Same object");
+else Console.WriteLine("Different");
